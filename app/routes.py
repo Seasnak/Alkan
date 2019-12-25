af2ea3b1ad1299d828a27bd8a0bcf18d444fa99e
Seasnak <sasank1020@gmail.com>
2019-12-25 00:15:04 -0800

    adding .gitignore, updating routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+#temp file for learning purposes
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index') #both '/' and '/index' will link to the following:
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mockey'} #mock user
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user) #collecting webpage from html template
